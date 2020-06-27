290fd78890c71cc280794b28da1ebaf1e4723cbc
Mathew Madhavacheril <mathewsyriac@gmail.com>
2020-06-26 21:35:28 -0400

    numpy install script

---
+++ scripts/install_numpy.py
@@ -0,0 +1,8 @@
+import os,sys
+
+vdict = {"3.6":"1.13.3","3.7":"1.14.5","3.8":"1.17.3"}
+version = ".".join(map(str, sys.version_info[:2]))
+np_version = vdict[version]
+cmd = f"pip3 install numpy=={np_version} -vvv"
+print("Running old-numpy install command: ", cmd)
+os.system(cmd)
