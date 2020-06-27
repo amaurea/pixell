2d561474df629dbe8d6680551436dbf2246087c1
Mathew Madhavacheril <mathewsyriac@gmail.com>
2020-06-26 23:03:10 -0400

    skip travis numpy version check

--- setup.py
+++ setup.py
@@ -79,7 +79,7 @@ but before testing.
 """
 NUMPY_VERSION=">=1.16" # Default numpy requirement
 bdeps = []
-if os.environ.get('CIBUILDWHEEL') or os.environ.get('TRAVIS'):
+if os.environ.get('CIBUILDWHEEL'):
     vdict = {"3.6":"1.13.3","3.7":"1.14.5","3.8":"1.17.3"}
     py_version = ".".join(map(str, sys.version_info[:2]))
     np_version = vdict[py_version]
